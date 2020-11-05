3f902dffb88d8114bd4bfb7ca4371ba6d86645b2
akirachr <akirachr@gmail.com>
2020-11-05 21:51:40 +0900

    Add files via upload

---
+++ 01.Element/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+// JSX produces an element.
+
+// Element
+const Element = (
+  <h1>Taro Nihon</h1>
+);
+
+ReactDOM.render(
+  Element,
+  document.getElementById('root')
+)
